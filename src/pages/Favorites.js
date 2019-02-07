91c9a3b2faa71963135abe23b3de73cd42898d69
Doctor Mew <trc379@gmail.com>
2019-02-06 20:49:26 -0800

    Added the beginning of the restaurant grabbing

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -15,19 +15,28 @@ componentDidMount() {
   const userFavs = [];
   Api.getFavorites(localStorage.getItem("userID")).then( ({data}) => {
     for(var i = 0; i < data.length; i++) {
-    userFavs.push(data[i].resId);
+    //userFavs.push(data[i].resId);
+    Api.getRestaurantByID(data[i].resId).then(({data}) => {
+      const joined = this.state.faveArray.concat(data);
+      this.setState({
+        faveArray : joined
+      })
+    });
   }
-  this.setState({
-    faveArray : userFavs
-  });
-});
-};
+})};
 
 render() {
   return(
     <div>
     <Navbar />
       <Container style={{ marginTop: 30 }}>
+      <ul>
+      {/* {props.groceries.map(item => (
+        <li className="list-group-item" key={item.id}>
+          {item.name}
+        </li>
+      ))} */}
+        </ul>
       </Container>
     </div>
   );
